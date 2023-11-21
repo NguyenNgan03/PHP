9ab8ffe78bc96c8e5695fa66d9168506a6534517
NguyenNgan03 <simple.270123@gmail.com>
2023-11-21 22:50:14 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<form action="post.php" method="post">
+    <label for=""> User Name: </label>
+    <input type="text" name="username">
+    <input type="submit" name="sub" value="Send">
+</form>
+
+   
+   
