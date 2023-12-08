50a8e4dc32ff848a853592cfec3ac6da08ac1dec
NguyenNgan03 <simple.270123@gmail.com>
2023-12-08 16:59:51 +0700

    NỘP BÀI

--- chapter_4/practices/s1/activity2/index.php
+++ chapter_4/practices/s1/activity2/index.php
@@ -4,37 +4,65 @@ require_once 'templates/header.php';
 
 <?php
 $host     = 'localhost'; // Because MySQL is running on the same computer as the web server
-$database = 'PHP_connect'; // Name of the database you use (you need first to CREATE DATABASE in MySQL)
+$database = 'demo'; // Name of the database you use (you need first to CREATE DATABASE in MySQL)
 $user     = 'root'; // Default username to connect to MySQL is root
-$password = ''; // Default password to connect to MySQL is empty
+$password = 'mysql'; // Default password to connect to MySQL is empty
 
 // TO DO: CREATE CONNECTION TO DATABASE
 // Read file: https://www.w3schools.com/php/php_mysql_connect.asp
-
-$db = 
+try {
+    $db = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    $sql = "CREATE TABLE posts (
+        id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name varchar(100) NOT NULL,
+        message varchar(200) NOT NULL
+      )";
+      $db->exec($sql);
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
 ?>
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['username']) && !empty($_POST['message'])) :
-    $username = $_POST['username'];
-    $message = $_POST['message'];
-    $db->query( // TO DO: INSERT NEW POST IN DATABASE
+    try {
+        $username = $_POST['username'];
+        $message = $_POST['message'];
+//     $db->query( // TO DO: INSERT NEW POST IN DATABASE
+        $sqlInsert = "INSERT INTO posts (name, message) VALUES (:name, :message)";
+        $stmt = $db ->prepare($sqlInsert);
+        $stmt -> bindParam(':name',$username);
+        $stmt -> bindParam(':message',$message);
+        $stmt->execute();
+        echo "isert data succesfully" ;
+    } catch (PDOException $e) {
+        echo "unsuccesful conect: ". $e -> getMessage();
+
+        //throw $th;
+    }
 endif;
 ?>
 
 <?php
 // TO DO: SELECT ALL POSTS FROM DATABASE
+    $selectQuery ="SELECT * FROM posts";
+    $result = $db -> query($selectQuery);
 
-$posts = 
+
+    $posts = $result -> fetchAll(PDO::FETCH_ASSOC);
+
 
 foreach ($posts as $post) :
 ?>
     <div class="card">
         <div class="card-header">
-            <span><?php // TO DO: display the value of username for this post ?></span>
+            <span><?php echo $post['name']; ?></span>
         </div>
         <div class="card-body">
-            <p class="card-text"><?php // TO DO: display the message for this post ?></p>
+            <p class="card-text"><?php echo $post['message']; ?></p>
         </div>
     </div>
     <hr>
